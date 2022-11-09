9f990a359683fbda1581a052d8ff9eeac2251772
Ravi Natarajan <ravinata@gmail.com>
2022-11-09 13:26:44 -0500

    updated code

--- app.py
+++ app.py
@@ -7,45 +7,63 @@ import requests
 headers = {
     'content-type': 'application/vnd.kafka.json.v2+json',
 }
-kafka_server = "http://my-bridge-bridge-service.openshift-operators.svc.cluster.local:8080"
-topic = "my-topic"
+
+kafka_consumer_group = "http://my-bridge-bridge-service.openshift-operators.svc.cluster.local:8080/consumers/my-topic-consumer-group"
+
+kafka_consumer_topic = "http://my-bridge-bridge-service.openshift-operators.svc.cluster.local:8080/consumers/my-topic-consumer-group/instances/my-topic-consumer/subscription"
+
+kafka_consumer_records = "http://my-bridge-bridge-service.openshift-operators.svc.cluster.local:8080/consumers/my-topic-consumer-group/instances/my-topic-consumer/records"
+
 
 def create_consumer():
-    
+    print("*** 1. Creating Bridge Consumer...")
+    data = {}
+    data['name'] = 'my-topic-consumer'
+    data['auto.offset.reset'] = 'earliest'
+    data['format'] = 'json'
+    data['enable.auto.commit'] = 'false'
+    data['fetch.min.bytes'] = 512
+    data['consumer.request.timeout.ms'] = 30000
+    json_data = json.dumps(data)
+
+    print("data to kafka:" + json_data)
+    response = requests.post(kafka_consumer_group, headers=headers, data=json_data)
+    print(response)
+    if (response.status_code) == 200:
+       subscribe2kafkaTopic()
+    else:
+       print("error creating consumer", response.txt)
+       exit()
 
 # subscribe to kafka topic my-topic
 def subscribe2kafkaTopic():
-    response = requests.post(kafka_server, headers=headers, data=data)
-    print(response)
+    print("*** 2. Subscribing to topic...")
+    data = {}
+    data['topics'] = ['my-topic']
+    json_data = json.dumps(data)
 
+    print("data to kafka:" + json_data)
+    response = requests.post(kafka_consumer_topic, headers=headers, data=json_data)
+    print(response)
+    if (response.status_code) == 200:
+       subscribe2kafkaTopic()
+    else:
+       print("error creating consumer", response.txt)
+       exit()
+    
+def listenAndProcess():
+    response = requests.get(kafka_consumer_records, headers=headers)
+    print(response)
+    if (response.status_code) == 200:
+       print("Successful get!")
+    else:
+       print("error creating consumer", response.txt)
+       exit()
+    
 def publish2S3(msg):
-    print("publishing to AWS S3")
+    print("3. Publishing to AWS S3")
 
 if __name__ == '__main__':
     create_consumer()
     subscribe2KafkaTopic()
-    
-    
-''' for reference
-    echo "1.creating bridge customer..."
-curl -X POST "http://my-bridge-bridge-service.openshift-operators.svc.cluster.local:8080/consumers/my-topic-consumer-group" \
-  -H 'content-type: application/vnd.kafka.v2+json' \
-  -d '{
-    "name": "my-topic-consumer",
-    "auto.offset.reset": "earliest",
-    "format": "json",
-    "enable.auto.commit": false,
-    "fetch.min.bytes": 512,
-    "consumer.request.timeout.ms": 30000
-  }'
-
-echo "2.subscribe to the topic as a customer..."
-curl -X POST http://my-bridge-bridge-service.openshift-operators.svc.cluster.local:8080/consumers/my-topic-consumer-group/instances/my-topic-consumer/subscription \
-  -H 'content-type: application/vnd.kafka.v2+json' \
-  -d '{
-    "topics": [
-        "my-topic"
-    ]
-}'
-'''
-    
+    listenAndProcess()
